63f8fd41a5550e36ec5f6a935faa3bcc05969bbf
Mushtaq M A <mushtaqat3gb@gmail.com>
2023-09-19 11:36:04 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace TypingSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.RichTextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(14, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(689, 425);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.Text = "";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(713, 11);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send Text";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBoxInput;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingSimulator
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon trayIcon;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize the system tray icon and context menu
+            trayIcon = new NotifyIcon();
+
+            trayIcon.Icon = new Icon("tray_stop.ico");// Replace with your custom icon
+            trayIcon.Visible = false;
+            trayIcon.Click += ExitApplication;
+
+            // Hide the main form
+            //this.WindowState = FormWindowState.Minimized;
+            //this.ShowInTaskbar = false;
+        }
+
+        private void SendTextToOtherApplication(object sender, EventArgs e)
+        {
+            // Get the text you want to send from a TextBox or any other source
+            string textToSend = textBoxInput.Text; // Replace with your source
+            //Hide the main form
+            trayIcon.Visible = true;
+            WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+            Thread.Sleep(5000);
+            // Use SendKeys to send the text to another application
+            foreach (char s in textToSend)
+            {
+                string ss = s.ToString();
+                if (s.ToString().Equals("("))
+                    ss = ss.Replace("(", "{(}");
+                if (s.ToString().Equals(")"))
+                    ss = ss.Replace(")", "{)}");
+                if (s.ToString().Equals("{"))
+                    ss = ss.Replace("{", "{{}");
+                if (s.ToString().Equals("}"))
+                    ss = ss.Replace("}", "{}}");
+                if (s.ToString().Equals("\n"))
+                    ss = ss.Replace("\n", "{ENTER}");
+                Thread.Sleep(200);
+                SendKeys.SendWait(ss);
+            }
+            trayIcon.Visible = false;
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+        }
+
+        private void ExitApplication(object sender, EventArgs e)
+        {
+            // Clean up and exit the application
+            trayIcon.Visible = false;
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+            //Application.Exit();
+            SendKeys.Flush();
+        }
+
+        // Rest of your form code
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            SendTextToOtherApplication(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendTextToOtherApplication(sender, e);
+        }
+    }
+}
